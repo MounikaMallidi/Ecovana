476fa625209d56d139ef000ccee063b203527356
M.Mounika <166210028+MounikaMallidi@users.noreply.github.com>
2025-11-12 21:09:26 +0530

    Update demoscript.js

--- demoscript.js
+++ demoscript.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 const API_KEY = "AIzaSyCA1JocTP_ZYo3fkKg3vwwVEBoR1oWnimA";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = { message:null, file:{ data:null, mime_type:null }};
 const chatHistory = [];
